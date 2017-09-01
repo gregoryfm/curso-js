b70f76b9a82d653adabd1015056a30ed347c409e
Gregory Menezes <gregoryfontenele@gmail.com>
2017-08-31 21:40:53 -0300

    Challenge 07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -8,14 +8,15 @@ Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 function addItem(item) {
-  return myArray.push(item);
+  myArray.push(item);
+  return myArray;
 }
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-addItem([1,true, '1']);
+console.log(addItem([1,true, '1']));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
